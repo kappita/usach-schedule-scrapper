2f3a31325c9833ee31ed1a41d64551b1515c38c0
Kappappita <ignacioladal@gmail.com>
2023-01-04 20:58:04 -0300

    chore: Modified some values, deleted some others due to refactoring.

--- values.py
+++ values.py
@@ -11,13 +11,12 @@ studentData = {
     "schedule": { day: {module : {"class" : str, "classroom": str} for module in range(1, 10)} for day in days }
 }
 
-
+personalClassesGap = 19
 xoffset = 5
 yoffset = 3
 yjump = 9
 xsize = 65
 ysize = 30
-correction = 5
+correction = 10
 testClassWidth = 16
-classY = 590
 
